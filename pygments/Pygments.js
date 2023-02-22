8509a1431a6dae57e5cd0d3b32dc4daac9fb1e0b
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2023-02-22 03:57:50 -1000

    Build

--- pygments/Pygments.js
+++ pygments/Pygments.js
@@ -1,95 +1,99 @@
-"use strict"
-Object.defineProperty(exports, "__esModule", { value: true })
-exports.PygmentsImporter = void 0
-const TrueBaseCrawler_1 = require("../TrueBaseCrawler")
-const { TreeNode } = require("jtree/products/TreeNode.js")
-const { Utils } = require("jtree/products/Utils.js")
-const cacheDir = __dirname + "/cache/"
-const { Disk } = require("jtree/products/Disk.node.js")
-const outputFile = cacheDir + "output.json"
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PygmentsImporter = void 0;
+const TrueBaseCrawler_1 = require("../TrueBaseCrawler");
+const { TreeNode } = require("jtree/products/TreeNode.js");
+const { Utils } = require("jtree/products/Utils.js");
+const cacheDir = __dirname + "/cache/";
+const { Disk } = require("jtree/products/Disk.node.js");
+const outputFile = cacheDir + "output.json";
 class PygmentsImporter extends TrueBaseCrawler_1.TrueBaseCrawler {
-  linkAllCommand() {
-    this.matches.forEach(entry => {
-      this.writeOne(entry.file, entry)
-    })
-  }
-  extractDataCommand() {
-    this.matches.forEach(entry => {
-      this.extractData(entry.file, entry)
-    })
-  }
-  extractData(file, entry) {
-    if (!file.has("pygmentsHighlighter")) return
-    if (!file.has("keywords") && entry.keywords.length)
-      file.set("keywords", entry.keywords.join(" "))
-    const nums = [
-      "Octals",
-      "Hexadecimals",
-      "Floats",
-      "Integers",
-      "BinaryNumbers"
-    ]
-    nums.forEach(num => {
-      const path = `features has${num}`
-      const value = entry[num.toLowerCase()]
-      const { lineCommentToken } = file
-      if (file.get(path) === undefined && value) {
-        file.set(path, "true")
-        if (lineCommentToken)
-          file.touchNode(path).setChildren(lineCommentToken + " " + value)
-      }
-    })
-    const features = ["hasLineComments", "hasMultiLineComments"]
-    features.forEach(feature => {
-      const path = `features ${feature}`
-      const value = entry[feature]
-      const { lineCommentToken } = file
-      if (file.get(path) === undefined && value) {
-        file.set(path, "true")
-      }
-    })
-    file.prettifyAndSave()
-  }
-  writeOne(file, entry) {
-    if (file.has("pygmentsHighlighter")) return
-    file.set("pygmentsHighlighter", entry.name)
-    file.set("pygmentsHighlighter filename", entry.filename)
-    const extensions = entry.extensions.join(" ")
-    if (extensions) file.set("pygmentsHighlighter fileExtensions", extensions)
-    file.prettifyAndSave()
-  }
-  get data() {
-    return Disk.readJson(outputFile)
-  }
-  get match() {
-    return this.data.map(entry => {
-      entry.extensions = entry.filenames.map(ext => ext.replace("*.", ""))
-      entry.filename = entry.filename.split("/").pop()
-      entry.trueBaseId =
-        this.base.searchForEntity(entry.name) ||
-        this.base.searchForEntityByFileExtensions(entry.extensions)
-      if (entry.trueBaseId) entry.file = this.base.getFile(entry.trueBaseId)
-      return entry
-    })
-  }
-  get matches() {
-    return this.match.filter(item => item.trueBaseId)
-  }
-  get misses() {
-    return this.match.filter(item => !item.trueBaseId)
-  }
-  addMissesCommand() {
-    this.misses
-      .filter(hit => hit.url)
-      .forEach(miss => {
-        const website = miss.url.includes("github")
-          ? `githubRepo ${miss.url}`
-          : `website ${miss.url}`
-        const newFile = this.base.createFile(`title ${miss.name}
+    linkAllCommand() {
+        this.matches.forEach(entry => {
+            this.writeOne(entry.file, entry);
+        });
+    }
+    extractDataCommand() {
+        this.matches.forEach(entry => {
+            this.extractData(entry.file, entry);
+        });
+    }
+    extractData(file, entry) {
+        if (!file.has("pygmentsHighlighter"))
+            return;
+        if (!file.has("keywords") && entry.keywords.length)
+            file.set("keywords", entry.keywords.join(" "));
+        const nums = [
+            "Octals",
+            "Hexadecimals",
+            "Floats",
+            "Integers",
+            "BinaryNumbers"
+        ];
+        nums.forEach(num => {
+            const path = `features has${num}`;
+            const value = entry[num.toLowerCase()];
+            const { lineCommentToken } = file;
+            if (file.get(path) === undefined && value) {
+                file.set(path, "true");
+                if (lineCommentToken)
+                    file.touchNode(path).setChildren(lineCommentToken + " " + value);
+            }
+        });
+        const features = ["hasLineComments", "hasMultiLineComments"];
+        features.forEach(feature => {
+            const path = `features ${feature}`;
+            const value = entry[feature];
+            const { lineCommentToken } = file;
+            if (file.get(path) === undefined && value) {
+                file.set(path, "true");
+            }
+        });
+        file.prettifyAndSave();
+    }
+    writeOne(file, entry) {
+        if (file.has("pygmentsHighlighter"))
+            return;
+        file.set("pygmentsHighlighter", entry.name);
+        file.set("pygmentsHighlighter filename", entry.filename);
+        const extensions = entry.extensions.join(" ");
+        if (extensions)
+            file.set("pygmentsHighlighter fileExtensions", extensions);
+        file.prettifyAndSave();
+    }
+    get data() {
+        return Disk.readJson(outputFile);
+    }
+    get match() {
+        return this.data.map(entry => {
+            entry.extensions = entry.filenames.map(ext => ext.replace("*.", ""));
+            entry.filename = entry.filename.split("/").pop();
+            entry.trueBaseId =
+                this.base.searchForEntity(entry.name) ||
+                    this.base.searchForEntityByFileExtensions(entry.extensions);
+            if (entry.trueBaseId)
+                entry.file = this.base.getFile(entry.trueBaseId);
+            return entry;
+        });
+    }
+    get matches() {
+        return this.match.filter(item => item.trueBaseId);
+    }
+    get misses() {
+        return this.match.filter(item => !item.trueBaseId);
+    }
+    addMissesCommand() {
+        this.misses
+            .filter(hit => hit.url)
+            .forEach(miss => {
+            const website = miss.url.includes("github")
+                ? `githubRepo ${miss.url}`
+                : `website ${miss.url}`;
+            const newFile = this.base.createFile(`title ${miss.name}
 type pl
-${website}`)
-        this.writeOne(newFile, miss)
-      })
-  }
+${website}`);
+            this.writeOne(newFile, miss);
+        });
+    }
 }
-exports.PygmentsImporter = PygmentsImporter
+exports.PygmentsImporter = PygmentsImporter;
