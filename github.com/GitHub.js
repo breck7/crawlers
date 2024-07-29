d5bc2a68c13d22aff86c8e98967142dc7ee74ea5
breck7 <breck7@gmail.com>
2024-07-29 15:21:24 +0000

    Deploying to scroll-output from @ breck7/crawlers@1ea271d1b9ba78165615dcd87a3871ff0a83e12a 🚀

--- github.com/GitHub.js
+++ github.com/GitHub.js
@@ -1,5 +1,7 @@
-import { PoliteCrawler, MeasurementsCrawler } from "../MeasurementsCrawler"
-
+"use strict"
+Object.defineProperty(exports, "__esModule", { value: true })
+exports.GitHubImporter = void 0
+const MeasurementsCrawler_1 = require("../MeasurementsCrawler")
 const { Utils } = require("jtree/products/Utils.js")
 const { TreeNode } = require("jtree/products/TreeNode.js")
 const superagent = require("superagent")
@@ -8,7 +10,6 @@ const dayjs = require("dayjs")
 const path = require("path")
 const YAML = require("yaml")
 const { Disk } = require("jtree/products/Disk.node.js")
-
 const cacheDir = path.join(__dirname, "cache")
 const reposDir = path.join(cacheDir, "repos")
 const repoCountCache = path.join(cacheDir, "repoCount")
@@ -16,12 +17,10 @@ const firstCommitCache = path.join(cacheDir, "firstCommits")
 const credsPath = path.join(__dirname, "ignore", "creds.json")
 const creds = JSON.parse(Disk.read(credsPath))
 const { apiToken, apiUser } = creds
-
 if (!apiToken) {
   console.error(`No GitHub token found`)
   process.exit()
 }
-
 const downloadJson = async (url, destination) => {
   const agent = superagent.agent()
   console.log(`downloading ${url}`)
@@ -31,34 +30,25 @@ const downloadJson = async (url, destination) => {
     .set("Authorization", `token ${apiToken}`)
   Disk.writeJson(destination, res.body || res.text || "")
 }
-
 Disk.mkdir(cacheDir)
 Disk.mkdir(reposDir)
 Disk.mkdir(firstCommitCache)
 Disk.mkdir(repoCountCache)
-
 class ConceptFileWithGitHub {
-  constructor(file: any, crawler: MeasurementsCrawler) {
+  constructor(file, crawler) {
     this.file = file
     this.crawler = crawler
   }
-
   get id() {
     return this.file.filename.replace(".scroll", "")
   }
-
-  file: any
-  crawler: MeasurementsCrawler
-
   get firstCommitResultPath() {
     return path.join(firstCommitCache, this.id + ".json")
   }
-
   async fetch() {
     await this.fetchFirstCommit()
     await this.downloadRepoInfo()
   }
-
   async downloadRepoInfo() {
     const { repoFilePath, userId, repoId } = this
     if (Disk.exists(repoFilePath)) return true
@@ -72,15 +62,12 @@ class ConceptFileWithGitHub {
       console.error(err)
     }
   }
-
   get githubLanguageId() {
     return this.languageNode.content
   }
-
   get repoCountPath() {
     return path.join(repoCountCache, `${this.githubLanguageId}.json`)
   }
-
   async fetchRepoCounts() {
     const { githubLanguageId, repoCountPath } = this
     if (Disk.exists(repoCountPath)) return
@@ -94,7 +81,6 @@ class ConceptFileWithGitHub {
       console.error(err)
     }
   }
-
   writeRepoCounts() {
     const { repoCountPath, languageNode, file } = this
     if (!Disk.exists(repoCountPath)) return this
@@ -102,23 +88,18 @@ class ConceptFileWithGitHub {
     languageNode.set("repos", obj.total_count.toString())
     file.prettifyAndSave()
   }
-
   get tree() {
     return this.crawler.getTree(this.file)
   }
-
   get githubNode() {
     return this.tree.getNode("githubRepo")
   }
-
   get languageNode() {
     return this.tree.getNode("githubLanguage")
   }
-
   get githubRepo() {
     return this.file.githubRepo.replace("https://github.com/", "")
   }
-
   async fetchTrending() {
     const { file, githubNode } = this
     const { fetchRepositories } = require("@huchenme/github-trending")
@@ -139,30 +120,23 @@ class ConceptFileWithGitHub {
       file.prettifyAndSave()
     })
   }
-
   get userId() {
     return this.githubRepo.split("/")[0]
   }
-
   get repoId() {
     return this.githubRepo.split("/")[1]
   }
-
   get repoFilePath() {
     return path.join(reposDir, `${this.userId}-${this.repoId}.json`)
   }
-
   writeRepoInfoToDatabase() {
     const { repoFilePath, file, tree } = this
     if (!Disk.exists(repoFilePath)) return this
     const obj = Disk.readJson(repoFilePath)
-
     if (typeof obj === "string") throw new Error("string:" + obj)
-
     if (!file.website && obj.homepage) {
       this.crawler.setAndSave(this.file, `website`, obj.homepage)
     }
-
     tree.getNode("githubRepo").setProperties({
       stars: obj.stargazers_count.toString(),
       forks: obj.forks.toString(),
@@ -176,17 +150,13 @@ class ConceptFileWithGitHub {
       // githubLanguage: obj.language,
       // githubHasWiki: obj.hasWiki,
     })
-
     this.crawler.save(file, tree)
     return this
   }
-
   async fetchFirstCommit() {
     const { file } = this
     if (Disk.exists(this.firstCommitResultPath)) return
-
     console.log(`Fetching "${file.id}"`)
-
     const url = file.githubRepo
     const parts = url.split("/")
     const repoName = parts.pop()
@@ -199,7 +169,6 @@ class ConceptFileWithGitHub {
         token: apiToken
         //sha: "5.0"
       })
-
       console.log(`Success for "${file.id}"`)
       console.log(commit)
       Disk.write(this.firstCommitResultPath, JSON.stringify(commit, null, 2))
@@ -209,11 +178,9 @@ class ConceptFileWithGitHub {
       Disk.write(this.firstCommitResultPath, `Error`)
     }
   }
-
   writeFirstCommitToDatabase() {
     const { file } = this
     if (file.githubRepo_firstCommit || !this.firstCommitFetched) return this
-
     try {
       const { firstCommit } = this
       const year = dayjs(firstCommit.commit.author.date).format("YYYY")
@@ -223,15 +190,12 @@ class ConceptFileWithGitHub {
     }
     return this
   }
-
   get firstCommitFetched() {
     return Disk.exists(this.firstCommitResultPath)
   }
-
   get firstCommit() {
     return JSON.parse(Disk.read(this.firstCommitResultPath))
   }
-
   autocompleteCreators() {
     const { file } = this
     try {
@@ -245,7 +209,6 @@ class ConceptFileWithGitHub {
     }
     return this
   }
-
   autocompleteAppeared() {
     const { file } = this
     const year = file.githubRepo_firstCommit
@@ -256,11 +219,10 @@ class ConceptFileWithGitHub {
     return this
   }
 }
-
-class GitHubImporter extends MeasurementsCrawler {
+class GitHubImporter extends MeasurementsCrawler_1.MeasurementsCrawler {
   async fetchAllRepoDataCommand() {
     console.log(`Fetching all...`)
-    const crawler = new PoliteCrawler()
+    const crawler = new MeasurementsCrawler_1.PoliteCrawler()
     crawler.maxConcurrent = 2
     await crawler.fetchAll(
       this.linkedFiles
@@ -268,7 +230,6 @@ class GitHubImporter extends MeasurementsCrawler {
         .map(file => new ConceptFileWithGitHub(file, this))
     )
   }
-
   get githubOfficiallySupportedLanguages() {
     // https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml
     return this.concepts
@@ -276,13 +237,12 @@ class GitHubImporter extends MeasurementsCrawler {
       .map(file => new ConceptFileWithGitHub(file, this))
       .reverse()
   }
-
   async fetchAllRepoCountsCommand() {
     const { githubOfficiallySupportedLanguages } = this
     console.log(
       `Fetching repo counts for all ${githubOfficiallySupportedLanguages.length} languages supported by GitHub...`
     )
-    const crawler = new PoliteCrawler()
+    const crawler = new MeasurementsCrawler_1.PoliteCrawler()
     crawler.maxConcurrent = 1
     crawler.msDelayBetweenRequests = 100
     await crawler.fetchAll(
@@ -290,13 +250,11 @@ class GitHubImporter extends MeasurementsCrawler {
       "fetchRepoCounts"
     )
   }
-
   writeAllRepoCountsCommand() {
     this.githubOfficiallySupportedLanguages.forEach(file =>
       file.writeRepoCounts()
     )
   }
-
   writeAllRepoDataCommand() {
     this.linkedFiles.forEach(file => {
       new ConceptFileWithGitHub(file, this)
@@ -306,7 +264,6 @@ class GitHubImporter extends MeasurementsCrawler {
         .autocompleteCreators()
     })
   }
-
   get langs() {
     const map = this.yamlMap
     return Object.keys(map).map(key => {
@@ -315,11 +272,9 @@ class GitHubImporter extends MeasurementsCrawler {
       return value
     })
   }
-
   get yamlMap() {
     return YAML.parse(Disk.read(path.join(cacheDir, "languages.yml")))
   }
-
   async writeLanguageDataCommand() {
     this.matched.forEach(match => {
       const { file, lang } = match
@@ -337,36 +292,28 @@ class GitHubImporter extends MeasurementsCrawler {
       ghNode.set("type", type)
       if (extensions)
         ghNode.set("fileExtensions", extensions.join(" ").replace(/\./g, ""))
-
       if (filenames) ghNode.set("filenames", filenames.join(" "))
-
       if (interpreters) ghNode.set("interpreters", interpreters.join(" "))
-
       if (aliases) {
         ghNode.delete("aliases")
-
         const delimiter = " or "
         Utils.ensureDelimiterNotFound(aliases, delimiter)
         ghNode.set("aliases", aliases.join(delimiter))
       }
-
       "ace_mode,codemirror_mode,codemirror_mime_type,tm_scope,wrap"
         .split(",")
         .forEach(key => {
           const value = lang[key]
           if (value) ghNode.set(key, value.toString())
         })
-
       if (group) {
         ghNode.set("group", group)
         // const conceptId = this.searchForConcept(group)
         // if (conceptId) ghNode.set("groupPldbId", conceptId)
       }
-
       file.prettifyAndSave()
     })
   }
-
   async writeLinksCommand() {
     this.matched.forEach(match => {
       const { file, lang } = match
@@ -378,24 +325,20 @@ class GitHubImporter extends MeasurementsCrawler {
       }
     })
   }
-
   get pairs() {
     return this.langs.map(lang => {
       const id = this.searchForConcept(lang.title)
       return { file: this.getFile(id), lang }
     })
   }
-
   get matched() {
     return this.pairs.filter(row => row.file)
   }
-
   get unmatched() {
     return this.pairs
       .filter(row => row.file === undefined)
       .map(item => item.lang)
   }
-
   listOutdatedLangsCommand() {
     const map = this.yamlMap
     this.concepts.forEach(file => {
@@ -404,17 +347,14 @@ class GitHubImporter extends MeasurementsCrawler {
         console.log(`Outdated: "${file.id}" has "${title}"`)
     })
   }
-
   listUnmatchedLangsCommand() {
     const missingPath = path.join(cacheDir, "missingLangs.json")
     Disk.write(missingPath, JSON.stringify(this.unmatched, null, 2))
     console.log(`Wrote ${this.unmatched.length} missing to: ${missingPath}`)
   }
-
   get linkedFiles() {
     return this.concepts.filter(file => file.githubRepo)
   }
-
   async runAll(file) {
     if (!file.githubRepo) return
     const gitFile = new ConceptFileWithGitHub(file, this)
@@ -425,5 +365,4 @@ class GitHubImporter extends MeasurementsCrawler {
       .autocompleteAppeared()
   }
 }
-
-export { GitHubImporter }
+exports.GitHubImporter = GitHubImporter
