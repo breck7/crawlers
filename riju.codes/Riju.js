3c204c03e9501681a74358d066c19ac84a2175d2
breck7 <breck7@gmail.com>
2024-07-29 15:24:53 +0000

    Deploying to scroll-output from @ breck7/crawlers@7a631f0bc6cb897b02e595d87ed9ccaf6b0cfd21 🚀

--- riju.codes/Riju.js
+++ riju.codes/Riju.js
@@ -1,82 +1,95 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RijuImporter = void 0;
-const MeasurementsCrawler_1 = require("../MeasurementsCrawler");
-const yaml_1 = require("yaml");
-const { Utils } = require("jtree/products/Utils.js");
-const { Disk } = require("jtree/products/Disk.node.js");
-const cachePath = __dirname + "/cache/riju/langs/";
-const scopeName = "rijuRepl";
-class RijuImporter extends MeasurementsCrawler_1.MeasurementsCrawler {
-    writeLinksToDatabaseCommand() {
-        this.matches.forEach(match => {
-            match.ConceptFile.set(scopeName, `https://riju.codes/${match.yaml.id}`);
-            match.ConceptFile.prettifyAndSave();
-        });
+"use strict"
+Object.defineProperty(exports, "__esModule", { value: true })
+exports.RijuImporter = void 0
+const MeasurementsCrawler = require("../MeasurementsCrawler")
+const yaml_1 = require("yaml")
+const { Utils } = require("jtree/products/Utils.js")
+const { Disk } = require("jtree/products/Disk.node.js")
+const cachePath = __dirname + "/cache/riju/langs/"
+const scopeName = "rijuRepl"
+class RijuImporter extends MeasurementsCrawler.MeasurementsCrawler {
+  writeLinksToDatabaseCommand() {
+    this.matches.forEach(match => {
+      match.ConceptFile.set(scopeName, `https://riju.codes/${match.yaml.id}`)
+      match.ConceptFile.prettifyAndSave()
+    })
+  }
+  get yamlFiles() {
+    return Disk.getFullPaths(cachePath).map(path =>
+      (0, yaml_1.parse)(Disk.read(path))
+    )
+  }
+  get matches() {
+    return this.yamlFiles
+      .map(yaml => {
+        const { id } = yaml
+        const match = this.searchForConcept(id)
+        if (match)
+          return {
+            ConceptFile: this.getFile(match),
+            yaml
+          }
+      })
+      .filter(i => i)
+  }
+  listMissingCommand() {
+    this.missing.map(yaml => console.log(`Missing language: ${yaml.id}`))
+  }
+  get missing() {
+    return this.yamlFiles.filter(yaml => !this.searchForConcept(yaml.id))
+  }
+  addMissingCommand() {
+    this.missing.forEach(yaml => {
+      var _a
+      const type =
+        ((_a = yaml.info) === null || _a === void 0 ? void 0 : _a.category) ===
+        "esoteric"
+          ? "esolang"
+          : "pl"
+      this.createFile(
+        `id ${yaml.name}
+tags ${type}`,
+        yaml.id
+      )
+    })
+  }
+  mergeOne(match) {
+    var _a, _b
+    const { ConceptFile, yaml } = match
+    const object = ConceptFile.toObject()
+    const { info } = yaml
+    const node = ConceptFile.getNode(scopeName)
+    if (yaml.template) node.appendLineAndChildren("example", yaml.template)
+    if (info) {
+      if (info.desc) node.set("description", info.desc)
+      if (info.year && !object.appeared)
+        ConceptFile.set("appeared", info.year.toString())
+      if (
+        ((_a = info.web) === null || _a === void 0 ? void 0 : _a.esolang) &&
+        !object.esolang
+      )
+        ConceptFile.set(
+          "esolang",
+          (_b = info.web) === null || _b === void 0 ? void 0 : _b.esolang
+        )
+      if (info.ext)
+        node.set(
+          "fileExtensions",
+          info.ext.join ? info.ext.join(" ") : info.ext
+        )
+      if (info.web.home) node.set("website", info.web.home)
+      if (info.web.source) node.set("githubRepo", info.web.source)
     }
-    get yamlFiles() {
-        return Disk.getFullPaths(cachePath).map(path => (0, yaml_1.parse)(Disk.read(path)));
-    }
-    get matches() {
-        return this.yamlFiles
-            .map(yaml => {
-            const { id } = yaml;
-            const match = this.searchForConcept(id);
-            if (match)
-                return {
-                    ConceptFile: this.getFile(match),
-                    yaml
-                };
-        })
-            .filter(i => i);
-    }
-    listMissingCommand() {
-        this.missing.map(yaml => console.log(`Missing language: ${yaml.id}`));
-    }
-    get missing() {
-        return this.yamlFiles.filter(yaml => !this.searchForConcept(yaml.id));
-    }
-    addMissingCommand() {
-        this.missing.forEach(yaml => {
-            var _a;
-            const type = ((_a = yaml.info) === null || _a === void 0 ? void 0 : _a.category) === "esoteric" ? "esolang" : "pl";
-            this.createFile(`id ${yaml.name}
-tags ${type}`, yaml.id);
-        });
-    }
-    mergeOne(match) {
-        var _a, _b;
-        const { ConceptFile, yaml } = match;
-        const object = ConceptFile.toObject();
-        const { info } = yaml;
-        const node = ConceptFile.getNode(scopeName);
-        if (yaml.template)
-            node.appendLineAndChildren("example", yaml.template);
-        if (info) {
-            if (info.desc)
-                node.set("description", info.desc);
-            if (info.year && !object.appeared)
-                ConceptFile.set("appeared", info.year.toString());
-            if (((_a = info.web) === null || _a === void 0 ? void 0 : _a.esolang) && !object.esolang)
-                ConceptFile.set("esolang", (_b = info.web) === null || _b === void 0 ? void 0 : _b.esolang);
-            if (info.ext)
-                node.set("fileExtensions", info.ext.join ? info.ext.join(" ") : info.ext);
-            if (info.web.home)
-                node.set("website", info.web.home);
-            if (info.web.source)
-                node.set("githubRepo", info.web.source);
-        }
-        ConceptFile.prettifyAndSave();
-    }
-    mergeInfoCommand() {
-        this.matches.forEach(match => {
-            try {
-                this.mergeOne(match);
-            }
-            catch (err) {
-                console.error(match.yaml.id, err);
-            }
-        });
-    }
+    ConceptFile.prettifyAndSave()
+  }
+  mergeInfoCommand() {
+    this.matches.forEach(match => {
+      try {
+        this.mergeOne(match)
+      } catch (err) {
+        console.error(match.yaml.id, err)
+      }
+    })
+  }
 }
-exports.RijuImporter = RijuImporter;
+exports.RijuImporter = RijuImporter
--- website/Website.js
+++ website/Website.js
@@ -1,150 +1,142 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WebsiteImporter = void 0;
-const cheerio = require("cheerio");
-const MeasurementsCrawler_1 = require("../MeasurementsCrawler");
-const { Utils } = require("jtree/products/Utils.js");
-const { TreeNode } = require("jtree/products/TreeNode.js");
-const lodash = require("lodash");
-const cacheDir = __dirname + "/cache/";
-const { Disk } = require("jtree/products/Disk.node.js");
-Disk.mkdir(cacheDir);
+"use strict"
+Object.defineProperty(exports, "__esModule", { value: true })
+exports.WebsiteImporter = void 0
+const cheerio = require("cheerio")
+const MeasurementsCrawler = require("../MeasurementsCrawler")
+const { Utils } = require("jtree/products/Utils.js")
+const { TreeNode } = require("jtree/products/TreeNode.js")
+const lodash = require("lodash")
+const cacheDir = __dirname + "/cache/"
+const { Disk } = require("jtree/products/Disk.node.js")
+Disk.mkdir(cacheDir)
 class Website {
-    constructor(file) {
-        this.file = file;
+  constructor(file) {
+    this.file = file
+  }
+  async update() {
+    const { file } = this
+    if (!file.website) return this
+    await this.download()
+    if (!Disk.exists(this.cachePath)) return
+    //this.extractText()
+  }
+  extractAll() {
+    if (!Disk.exists(this.cachePath)) return
+    this.extractSocial("twitter", "twitter")
+    this.extractSocial("instagram", "instagram")
+    this.extractSocial("facebook", "facebook")
+    this.extractSocial("pinterest", "pinterest")
+    this.extractSocial("youtube", "youTubeChannel", "user/")
+    this.extractSocial("linkedin", "linkedin", "company/")
+    this.extractPhoneNumber()
+    this.extractGitHub()
+    //this.extractTitle()
+    this.file.prettifyAndSave()
+  }
+  get cachePath() {
+    return cacheDir + this.file.id + ".html"
+  }
+  async download() {
+    const { cachePath } = this
+    if (Disk.exists(cachePath)) return this
+    try {
+      console.log("downloading to " + cachePath)
+      const result = await fetch(this.file.get("website"))
+      const text = await result.text()
+      Disk.write(cachePath, text)
+    } catch (err) {
+      console.error(err)
     }
-    async update() {
-        const { file } = this;
-        if (!file.website)
-            return this;
-        await this.download();
-        if (!Disk.exists(this.cachePath))
-            return;
-        //this.extractText()
+  }
+  get content() {
+    return Disk.read(this.cachePath)
+  }
+  extractGitHub() {
+    const { file } = this
+    if (file.githubRepo) return this
+    let potentialHits = Utils.getLinks(this.content.toLowerCase()).filter(
+      link => {
+        link = link.trim()
+        const path = new URL(link).pathname.split("/")
+        return link.includes("https://github.com") && path.length === 3
+      }
+    )
+    //todo: improve
+    potentialHits = lodash.uniq(potentialHits)
+    if (potentialHits.length === 1)
+      file.set(`githubRepo`, potentialHits[0].trim())
+    return this
+  }
+  extractTitle() {
+    const { file, content } = this
+    const cheer = cheerio.load(content)
+    let title = ""
+    cheer("title").each((i, elem) => {
+      file.set("website title", cheer(elem).text())
+    })
+    return this
+  }
+  extractText() {
+    // todo
+  }
+  extractPhoneNumber() {
+    const { file } = this
+    if (file.phoneNumber) return
+    if (!Disk.exists(this.cachePath)) return
+    const matches = this.content.match(/(1-\d\d\d-\d\d\d-\d\d\d\d)/)
+    if (matches) file.set("phoneNumber", matches[0])
+    return this
+  }
+  extractSocial(name, propName, basePath = "") {
+    const { file } = this
+    if (file.has(propName)) return
+    const str = this.content.toLowerCase()
+    const { id } = file
+    if (str.includes(name)) {
+      // todo: update
+      const regex = new RegExp(`${name}\.com\/${basePath}([^"\/ \?\']+)`, "g")
+      const matches = str.match(regex)
+      if (matches) {
+        matches
+          .map(m => m.replace(`${name}.com/${basePath}`, ""))
+          .forEach(m => {
+            if (
+              m === id ||
+              m.includes(id) ||
+              id.includes(m) ||
+              file.toString().includes(m)
+            ) {
+              file.set(propName, `https://${name}.com/${basePath}${m}`)
+            }
+          })
+      }
+      let out = ""
+      if (matches) out = matches.join(" ")
+      console.log(`${id} ${out}`)
     }
-    extractAll() {
-        if (!Disk.exists(this.cachePath))
-            return;
-        this.extractSocial("twitter", "twitter");
-        this.extractSocial("instagram", "instagram");
-        this.extractSocial("facebook", "facebook");
-        this.extractSocial("pinterest", "pinterest");
-        this.extractSocial("youtube", "youTubeChannel", "user/");
-        this.extractSocial("linkedin", "linkedin", "company/");
-        this.extractPhoneNumber();
-        this.extractGitHub();
-        //this.extractTitle()
-        this.file.prettifyAndSave();
-    }
-    get cachePath() {
-        return cacheDir + this.file.id + ".html";
-    }
-    async download() {
-        const { cachePath } = this;
-        if (Disk.exists(cachePath))
-            return this;
+    return this
+  }
+}
+class WebsiteImporter extends MeasurementsCrawler.MeasurementsCrawler {
+  get matches() {
+    return this.concepts
+      .filter(file => file.website)
+      .map(file => new Website(file))
+  }
+  async downloadAllCommand() {
+    await Promise.all(this.matches.map(file => file.download()))
+  }
+  async updateAllCommand() {
+    lodash
+      .shuffle(this.concepts.filter(file => file.website))
+      .forEach(async file => {
         try {
-            console.log("downloading to " + cachePath);
-            const result = await fetch(this.file.get("website"));
-            const text = await result.text();
-            Disk.write(cachePath, text);
+          await new Website(file).update()
+        } catch (err) {
+          console.error(err)
+          console.log(`Error with ${file.id}`)
         }
-        catch (err) {
-            console.error(err);
-        }
-    }
-    get content() {
-        return Disk.read(this.cachePath);
-    }
-    extractGitHub() {
-        const { file } = this;
-        if (file.githubRepo)
-            return this;
-        let potentialHits = Utils.getLinks(this.content.toLowerCase()).filter(link => {
-            link = link.trim();
-            const path = new URL(link).pathname.split("/");
-            return link.includes("https://github.com") && path.length === 3;
-        });
-        //todo: improve
-        potentialHits = lodash.uniq(potentialHits);
-        if (potentialHits.length === 1)
-            file.set(`githubRepo`, potentialHits[0].trim());
-        return this;
-    }
-    extractTitle() {
-        const { file, content } = this;
-        const cheer = cheerio.load(content);
-        let title = "";
-        cheer("title").each((i, elem) => {
-            file.set("website title", cheer(elem).text());
-        });
-        return this;
-    }
-    extractText() {
-        // todo
-    }
-    extractPhoneNumber() {
-        const { file } = this;
-        if (file.phoneNumber)
-            return;
-        if (!Disk.exists(this.cachePath))
-            return;
-        const matches = this.content.match(/(1-\d\d\d-\d\d\d-\d\d\d\d)/);
-        if (matches)
-            file.set("phoneNumber", matches[0]);
-        return this;
-    }
-    extractSocial(name, propName, basePath = "") {
-        const { file } = this;
-        if (file.has(propName))
-            return;
-        const str = this.content.toLowerCase();
-        const { id } = file;
-        if (str.includes(name)) {
-            // todo: update
-            const regex = new RegExp(`${name}\.com\/${basePath}([^"\/ \?\']+)`, "g");
-            const matches = str.match(regex);
-            if (matches) {
-                matches
-                    .map(m => m.replace(`${name}.com/${basePath}`, ""))
-                    .forEach(m => {
-                    if (m === id ||
-                        m.includes(id) ||
-                        id.includes(m) ||
-                        file.toString().includes(m)) {
-                        file.set(propName, `https://${name}.com/${basePath}${m}`);
-                    }
-                });
-            }
-            let out = "";
-            if (matches)
-                out = matches.join(" ");
-            console.log(`${id} ${out}`);
-        }
-        return this;
-    }
+      })
+  }
 }
-class WebsiteImporter extends MeasurementsCrawler_1.MeasurementsCrawler {
-    get matches() {
-        return this.concepts
-            .filter(file => file.website)
-            .map(file => new Website(file));
-    }
-    async downloadAllCommand() {
-        await Promise.all(this.matches.map(file => file.download()));
-    }
-    async updateAllCommand() {
-        lodash
-            .shuffle(this.concepts.filter(file => file.website))
-            .forEach(async (file) => {
-            try {
-                await new Website(file).update();
-            }
-            catch (err) {
-                console.error(err);
-                console.log(`Error with ${file.id}`);
-            }
-        });
-    }
-}
-exports.WebsiteImporter = WebsiteImporter;
+exports.WebsiteImporter = WebsiteImporter
